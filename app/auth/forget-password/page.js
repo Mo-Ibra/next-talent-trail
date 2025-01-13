0c7ac8cd953779505f01c34c0eb2117e3d6d856d
Mo-Ibra <mohamedelgedwy245@gmail.com>
2025-01-13 23:49:20 +0200

    Added Auth

---
+++ app/auth/forget-password/page.js
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Forget Password</h1>
+    </div>
+  )
+}
+
+export default Page;
---
+++ app/contact/page.js
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+    </div>
+  )
+}
+
+export default Page;
